5ab1386827f582556468bf5af43a7e7018f0e1da
chu83 <59534807+chu83@users.noreply.github.com>
2020-11-18 14:22:13 +0900

    Module

--- 08/ex01.py
+++ 08/ex01.py
@@ -1,54 +1,35 @@
 #기울기(gradient)
-
+import os
+import sys
+from pathlib import Path
 import numpy as np
 
-def f(x):
+try:
+    #print(os.getcwd())
+    #sys.path.append('F:\deep-learning\dowork\PycharmProjects\linear-algebra-basics\lib')
+    #p1 = Path(os.getcwd()).parent + '\\lib'
+    #p2 = os.path.join(Path(os.getcwd()).parent, 'lib')
+    #print(p2)
+    #sys.path.append(p2)
 
+    sys.path.append(os.path.join(Path(os.getcwd()).parent, 'lib'))
+
+    #import common as cm
+    from common import numerical_gradient
+
+except ImportError:
+    print('Library Module Can Not Found')
+
+
+def f(x):
     return np.sum(x**2, axis = 0)
 
-s1 = f(np.array(10))
-s2 = f(np.array([3.,4.]))
-s3 = f(np.array([3,4,5]))
-print(s1)
-print(s2)
-print(s3)
+#함수테스트
 
 
-def numerical_gradient(f, x):
-    h = 1e-4
-    gradient = np.zeros_like(x)
-    #x = [3, 4]
-    # h1 = f(np.array([x[0]+h], x[1))
-    # h2 = f(np.array([x[0]-h], x[1]))
-    # gradient[0] = (h1-h2) / (2*h)
-    #
-    # h1 = f(np.array([x[0], x[1]+h]))
-    # h2 = f(np.array([x[0], x1[1]-h]))
-    # gradient[1] = (h1 - h2) / (2 * h)
-
-    for i in range(x.size):
-        tmp = x[i]
-        x[i] = x[i] + h
-        h1 = f(x)
+gra1 = numerical_gradient(f, np.array([3., 4.]))
+gra2 = numerical_gradient(f, np.array([0., 2.]))
+gra3 = numerical_gradient(f, np.array([3., 0.]))
 
 
-        x[i] = tmp - h
-        h2 = f(x)
-
-        gradient[i] = (h1-h2) / (2*h)
-        x[i] = tmp
-
-    return gradient
-#함수 테스트
-#print(f(np.array([3, 4])))
-
-gra1 = numerical_gradient(f, np.array(f, np.array[3., 4.]))
-gra2 = numerical_gradient(f, np.array(f, np.array[0., 2.]))
-gra3 = numerical_gradient(f, np.array(f, np.array[3., 0.]))
-
-
-
-
-
-
-
+print(gra1, gra2, gra3)
